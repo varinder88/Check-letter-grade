e1d451ad05a07a649a255b7efc6ef7a3a67f5aaa
varinder88 <varinderveer87@gmail.com>
2022-05-29 04:53:02 -0700

    Initial Commit

---
+++ Lab1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab1
+//
+//  Created by Varinder Toor on 2022-05-29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
